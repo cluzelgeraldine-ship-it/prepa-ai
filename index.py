41a930c8c4159aa14b4e8a4191c3bfc31d57e13e
cluzelgeraldine-ship-it <cluzelgeraldine@gmail.com>
2026-04-12 12:25:26 +0200

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ import openai
 app = FastAPI()
 
 # 1. TA CLÉ API
-openai.api_key = "os.getenv("OPENAI_API_KEY")"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # 2. MODÈLE DE DONNÉES
 class Message(BaseModel):
